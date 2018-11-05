9757974dfcef5872a9c028eee981b359550f3839
rezera <071bct538@pcampus.edu.np>
2018-11-05 13:29:23 +0545

    Floor + yolo

--- floor.py
+++ floor.py
@@ -1,37 +1,66 @@
 import numpy as np
 import cv2
 import math
+from PIL import Image
+import os 
 
-img = cv2.imread("download.png")
-height, width = img.shape[:2]
-grayscale = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-edges = cv2.Canny(grayscale, 75, 150, apertureSize=3, L2gradient=True)
-lines = cv2.HoughLines(edges, 1, np.pi/180, 150 , 1)
+counter = 0
+for root, directory, files in os.walk('test/input/'):
+    for img in files:
+        im = Image.open(os.path.join(root,img))
+        image = np.array(im)
+        print('Original Dimensions : ',image.shape)
+        height = 640
+        (h,w) = image.shape[:2]
+        r = height / float(h)
+        width = int(w*r)
+        dim = (width, height)
+        # resize image
+        resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+        print('Resized Dimensions : ',resized.shape)
+        grayscale = cv2.cvtColor(resized, cv2.COLOR_RGB2GRAY)
+        sigma = 0.33
+        v = np.median(resized)
+        # apply automatic Canny edge detection using the computed median
+        # lower = int(max(0, (1.0 - sigma) * v))
+        # upper = int(min(255, (1.0 + sigma) * v))
+        edges = cv2.Canny(grayscale, 100, 150, apertureSize=3, L2gradient=True)
+        lines = cv2.HoughLines(edges, 1, np.pi/180, 150) 
+        lines1 = []
+        for i in range(len(lines)):
+            for j in range(len(lines)):
+                if (i == j):continue
+                if (abs(lines[i][0][1] - lines[j][0][1]) == 0):          
+                    #You've found a parallel line!
+                    lines1.append(lines[i])
+                    lines1.append(lines[j])
 
-for line in lines:
-    rho, theta= line[0]
-    # Stores the value of cos(theta) in a 
-    a = np.cos(theta) 
-    # Stores the value of sin(theta) in b 
-    b = np.sin(theta) 
-    # x0 stores the value rcos(theta) 
-    x0 = a*rho
-    # y0 stores the value rsin(theta) 
-    y0 = b*rho
-    # x1 stores the rounded off value of (rcos(theta)-1000sin(theta)) 
-    x1 = int(x0 + width*(-b)) 
-    # y1 stores the rounded off value of (rsin(theta)+1000cos(theta)) 
-    y1 = (y0 + width*(a)) 
-    # x2 stores the rounded off value of (rcos(theta)+1000sin(theta)) 
-    x2 = int(x0 - width*(-b)) 
-    # y2 stores the rounded off value of (rsin(theta)-1000cos(theta)) 
-    y2 = (y0 - width*(a)) 
-    # cv2.line draws a line in img from the point(x1,y1) to (x2,y2). 
-    # (0,255,0) denotes the colour of the line to be  
-    #drawn. In this case, it is green.  
-    cv2.line(img,(x1,int(y1)), (x2,int(y2)), (0,255,0),2)  
+        for line in lines1:
+            rho, theta= line[0]
+            # Stores the value of cos(theta) in a 
+            a = np.cos(theta) 
+            # Stores the value of sin(theta) in b 
+            b = np.sin(theta) 
+            # x0 stores the value rcos(theta) 
+            x0 = a*rho
+            # y0 stores the value rsin(theta) 
+            y0 = b*rho
+            # x1 stores the rounded off value of (rcos(theta)-1000sin(theta)) 
+            x1 = int(x0 + width*(-b)) 
+            # y1 stores the rounded off value of (rsin(theta)+1000cos(theta)) 
+            y1 = (y0 + width*(a)) 
+            # x2 stores the rounded off value of (rcos(theta)+1000sin(theta)) 
+            x2 = int(x0 - width*(-b)) 
+            # y2 stores the rounded off value of (rsin(theta)-1000cos(theta)) 
+            y2 = (y0 - width*(a)) 
+            # find the slop of each line
+            slope = (y2-y1)/(x2-x1)
+            #dictionary of lines
+            # cv2.line draws a line in img from the point(x1,y1) to (x2,y2). 
+            # (0,255,0) denotes the colour of the line to be  
+            #drawn. In this case, it is green.  
+            if slope != float('-Inf'):
+                cv2.line(resized,(x1,int(y1)), (x2,int(y2)), (0,255,0),1)  
 
-cv2.imshow("Image", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
+        filename = "test/output/"+ os.path.splitext(img)[0]+".jpg"
+        cv2.imwrite(filename, resized)
--- yolo_video.py
+++ yolo_video.py
@@ -2,51 +2,26 @@ import sys
 import argparse
 import cv2
 import math
+import os
 import numpy as np
 from yolo import YOLO, detect_video
 from PIL import Image
 
 def detect_img(yolo):
     while True:
-        img = input('Input image filename:')
-        try:
-            image = Image.open(img)
-        except:
-            print('Open Error! Try again!')
-            continue
-        else:
-            r_image = yolo.detect_image(image)
-            t_image = np.array(r_image)
-            height, width = t_image.shape[:2]
-            grayscale = cv2.cvtColor(t_image, cv2.COLOR_RGB2GRAY)
-            edges = cv2.Canny(grayscale, 75, 150, apertureSize=3)
-            lines = cv2.HoughLines(edges, 1, np.pi/180, 250 , 1)
-
-            for line in lines:
-                rho, theta= line[0]
-                # Stores the value of cos(theta) in a 
-                a = np.cos(theta) 
-                # Stores the value of sin(theta) in b 
-                b = np.sin(theta) 
-                # x0 stores the value rcos(theta) 
-                x0 = a*rho
-                # y0 stores the value rsin(theta) 
-                y0 = b*rho
-                # x1 stores the rounded off value of (rcos(theta)-1000sin(theta)) 
-                x1 = int(x0 + width*(-b)) 
-                # y1 stores the rounded off value of (rsin(theta)+1000cos(theta)) 
-                y1 = (y0 + width*(a)) 
-                # x2 stores the rounded off value of (rcos(theta)+1000sin(theta)) 
-                x2 = int(x0 - width*(-b)) 
-                # y2 stores the rounded off value of (rsin(theta)-1000cos(theta)) 
-                y2 = (y0 - width*(a)) 
-                # cv2.line draws a line in img from the point(x1,y1) to (x2,y2). 
-                # (0,255,0) denotes the colour of the line to be  
-                #drawn. In this case, it is green.  
-                cv2.line(t_image,(x1,int(y1)), (x2,int(y2)), (0,255,0),2)  
-            cv2.imshow("Image", t_image)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
+        for root, directory, files in os.walk('test/input/'):
+            for img in files:
+                image = np.array(Image.open(os.path.join(root,img)))
+                height = 640
+                (h,w) = image.shape[:2]
+                r = height / float(h)
+                width = int(w*r)
+                dim = (width, height)
+                # resize image
+                resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+                r_image = yolo.detect_image(resized)
+                filename = "test/output_yolo/"+ os.path.splitext(img)[0]+".jpg"
+                cv2.imwrite(filename, r_image)
     yolo.close_session()
 
 FLAGS = None
